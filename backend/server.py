0ba90da8fcc61bc38782abab7da7e761b3c88c86
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2024-01-20 18:42:51 -0500

    fixing server

--- backend/server.py
+++ backend/server.py
@@ -39,13 +39,15 @@ def signup():
 
 @app.route('/login', methods=['POST'])
 def login():
-    user = request.json.get('user', None)
-    pw = request.json.get('password', None)
+    user = request.json['user']
+    pw = request.json['password']
     if not user or not pw:
         return jsonify({'error': 'Bad username or password'}), 401
-    result = mongo.db.findOne({'user': user, 'password': pw})
+    data = mongo.db.find_one({'user': user, 'password': pw})
+    if not data:
+        return jsonify({'error': 'Bad username or password'}), 401
     access_token = create_access_token(identity=user)
-    return jsonify(access_token=access_token)
+    return jsonify(access_token=access_token), 201
 
 
 if __name__ == '__main__':
