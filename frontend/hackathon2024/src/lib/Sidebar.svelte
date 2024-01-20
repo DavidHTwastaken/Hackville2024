b93998e98b1cff70e7751739d15970e095d472f1
Dean <deang467@gmail.com>
2024-01-20 14:31:01 -0500

    Sidebar not complete

---
+++ frontend/hackathon2024/src/lib/Sidebar.svelte
@@ -0,0 +1,13 @@
+<ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active" href="/src/routes/flirts/flirts.svelte">Learn</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/src/routes/chatbot/chatbot.svelte">Chatbot</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/src/routes/preferences/preferences.svelte"
+      >Preferences</a
+    >
+  </li>
+</ul>
