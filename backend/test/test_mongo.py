01fd02c0f27e3275d9596e0be3d34ba7e8f1897c
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2024-01-20 17:27:23 -0500

    mongo testing

---
+++ backend/test/test_mongo.py
@@ -0,0 +1,19 @@
+import os
+from flask_pymongo import PyMongo
+from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = "mongodb://localhost:27017/hackville2024"
+mongo = PyMongo(app)
+
+@app.route('/test',methods=['POST'])
+def test():
+    mongo.db.create_collection('users')
+    mongo.db.users.insert_one({'name': 'Jakob'})
+    data = mongo.db.users.find_one({'name':'Jakob'})
+    print(data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
