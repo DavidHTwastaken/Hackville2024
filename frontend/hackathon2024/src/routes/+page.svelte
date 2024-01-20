f8c522a0db4457f6f7ccf52ba2074d504956f796
Dean <deang467@gmail.com>
2024-01-20 13:47:58 -0500

    Main page and top nav bar

--- frontend/hackathon2024/src/routes/+page.svelte
+++ frontend/hackathon2024/src/routes/+page.svelte
@@ -1,11 +1,87 @@
-<script>
-
-</script>
-
-<main>
-    <h1>Welcome to my page!</h1>
-</main>
-
-<style>
-
-</style>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <title>Document</title>
+  </head>
+  <body>
+    <nav
+      class="navbar d-flex navbar-expand-lg"
+      style="background-color: #b6b6b6"
+    >
+      <div class="container-fluid">
+        <div
+          class="collapse navbar-collapse d-flex justify-content-center"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav nav-fill w-100">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">How it works</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About US</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div class="container-fluid">
+      <div class="d-flex justify-content-evenly" style="padding-top: 2em">
+        <span
+          class="btn btn-outline-secondary"
+          style="padding-top: 1em; padding-bottom: 1em"
+          >New to the country?</span
+        >
+        <span
+          class="btn btn-outline-secondary"
+          style="padding-top: 1em; padding-bottom: 1em"
+          >Cultural unawareness?</span
+        >
+        <span
+          class="btn btn-outline-secondary"
+          style="padding-top: 1em; padding-bottom: 1em"
+          >Hesist about dating?</span
+        >
+        <span
+          class="btn btn-outline-secondary"
+          style="padding-top: 1em; padding-bottom: 1em"
+          >Need help with practicing flirting?
+        </span>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div
+        class="d-flex justify-content-center"
+        style="padding-left: 33%; padding-right: 33%"
+      >
+        <div style="text-align: center; padding-top: 33%">
+          <h1>Flirt & Learn</h1>
+          <h3>where dating comes true</h3>
+          <p>
+            Try out our new AI-Powered Flirt Coach which helps you practice
+            flirting with real-time feedback and personalized lessons
+          </p>
+          <button class="btn btn-outline-secondary">TRY NOW!</button>
+        </div>
+      </div>
+    </div>
+    <footer style="background-color: #b6b6b6"></footer>
+  </body>
+</html>
