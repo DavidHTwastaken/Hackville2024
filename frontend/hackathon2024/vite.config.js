6f843fc71cdd70b601338534c77885b14051de1c
David Houle-Tymeczko <david.houletymeczko@ontariotechu.net>
2024-01-21 09:32:54 -0500

    Fix url

--- frontend/hackathon2024/vite.config.js
+++ frontend/hackathon2024/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       // Proxying API requests to Flask backend
       "/api": {
-        target: "http://127.0.0.1:5000", // Flask runs on port 5000 by default
+        target: "https://hackville2024.ue.r.appspot.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
