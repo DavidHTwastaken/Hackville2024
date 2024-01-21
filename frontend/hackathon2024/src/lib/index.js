a3b70a0b3b8ec272fd6f92a07aec5461ee907245
obrdanie <72950043+obrdanie@users.noreply.github.com>
2024-01-20 19:43:36 -0500

    Added HTMX
    Someone took out htmx

--- frontend/hackathon2024/src/lib/index.js
+++ frontend/hackathon2024/src/lib/index.js
@@ -1,1 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as Sidebar } from './Sidebar'
+export { default as Navbar } from './Navbar'
--- frontend/hackathon2024/src/routes/+page.svelte
+++ frontend/hackathon2024/src/routes/+page.svelte
@@ -1,23 +1,6 @@
 <script>
   import Navbar from "$lib/Navbar.svelte";
-  import Sidebar from "$lib/+Sidebar.svelte";
 </script>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-      crossorigin="anonymous"
-    />
-    <title>Document</title>
-  </head>
-  <body>
-    <Sidebar />
     <Navbar />
     <div class="container-fluid">
       <div class="d-flex justify-content-evenly" style="padding-top: 2em">
@@ -60,4 +43,4 @@
       </div>
     </div>
     <footer style="background-color: #b6b6b6"></footer>
-  </main>
+
